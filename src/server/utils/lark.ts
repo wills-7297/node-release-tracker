5670db40212212fe58f2259293e25c1f1536bb89
sh07017ml <sh07017ml@sh07017mldeMacBook-Pro.local>
2022-04-18 11:55:08 +0800

    增加节点可视化admin群

--- src/server/utils/lark.ts
+++ src/server/utils/lark.ts
@@ -1,4 +1,4 @@
-export const CORRESPONDENT_LARK_URL = "https://open.larksuite.com/open-apis/bot/v2/hook/4374b8b5-bd42-4ff1-b020-194e33fa728a";
+export const CORRESPONDENT_LARK_URL = "https://open.larksuite.com/open-apis/bot/v2/hook/816f1422-032f-497e-b463-7fefcdff1390";
 
 export const generateLarkNotice = (title, array) => {
     let content: any[] = [];
